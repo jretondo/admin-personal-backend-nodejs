36c6724bca219795762f23731774e5aa23f7347f
jretondo <jretondo90@gmail.com>
2021-09-24 17:26:40 -0300

    add secure

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const fs = require('fs')
+const https = require('https')
 require('dotenv').config({
     path: path.join(__dirname, "..", ".env")
 })
