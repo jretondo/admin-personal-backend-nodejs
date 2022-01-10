29a13790e057cad0657be377e100deb9bf7afd1a
jretondo <jretondo90@gmail.com>
2022-01-10 01:32:32 -0300

    add email

--- api/components/emails/network.js
+++ api/components/emails/network.js
@@ -5,7 +5,7 @@ const Controller = require("./index")
 
 //internal Functions
 const sendEmail = (req, res, next) => {
-    Controller.sendEmail(req.query.body)
+    Controller.sendEmail(req.body)
         .then((text) => {
             response.success(req, res, 200, text)
         })
