8d814aea95d8430f040c2f51e92c50da49b9ae9d
jretondo <jretondo90@gmail.com>
2022-01-10 01:30:34 -0300

    add email

--- api/components/emails/network.js
+++ api/components/emails/network.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const response = require("../../../network/response")
+const Controller = require("./index")
 
 //internal Functions
 const sendEmail = (req, res, next) => {
